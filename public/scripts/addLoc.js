84766785439f50cb7eb9d21b3ea26535729f8e4a
grvcodes <gurv0002@gmail.com>
2019-10-05 17:43:10 +0530

    new loader for addLoc

--- public/scripts/addLoc.js
+++ public/scripts/addLoc.js
@@ -1,16 +1,16 @@
 let form = document.querySelector("form")
 let input = document.querySelector("input");
-let loader = document.querySelector(".loader")
 let wrapperTo = document.querySelector(".wrapperTo")
 let fallback = document.querySelector("div.fallback")
 let tabPage = document.querySelector("div.tab")
 
+let loader = document.createElement('div');
+loader.classList.add('new-loader');
 
 let flag = false
 let state = [];
 class LocStorage {
     constructor(){
-        console.log("obj");
         if(localStorage.getItem('locations')){
             flag = true;
             state = localStorage.getItem('locations').split("@")
@@ -57,75 +57,51 @@ let tab= (loc,i)=>{
     return html;
 }
 
-function hasClass(element,className){
-    let x= element.classList;
-    for(i=0;i<element.classList.length;i++){
-        if(x[i]==className){
-            console.log("have this class")
-            return true
-        }
-    }
-    return false
-}
-
 form.addEventListener("submit",(e)=>{
 
     e.preventDefault();
     fallback.textContent = "";
-    /*remove hide class to loader if present*/
-    if((hasClass(loader,'hide'))){
-        loader.classList.toggle("hide");
-    }
-    /*add hide class to data if class removed from it by previous display of data*/
-    if(!(hasClass(wrapperTo,'hide'))){
-        wrapperTo.classList.toggle("hide");
-    }
-    
+    tabPage.appendChild(loader).textContent="Loading...";
     let location = input.value;
     if(!location){
         fallback.textContent = "please enter a location";
-        loader.classList.toggle("hide");
+        tabPage.removeChild(loader);
     }else{
         fetch("/location?q="+location).then((res)=>{
             res.json().then((data)=>{
-                loader.classList.toggle("hide");/*add hide class to hide the loader*/
-                wrapperTo.classList.toggle("hide");/*remove hide class to display data ehen available*/
                 if(data.error){
+                    tabPage.removeChild(loader);
                     fallback.textContent= data.error;
                     return
                 }
-        
-                    let loc = data.features;
-                    tabPage.innerHTML="";
-                    for(let i=0;i<loc.length;i++){
-                        let div = document.createElement("div");
-                        div.innerHTML = tab(loc[i].place_name,i);
-                        tabPage.appendChild(div);
-
-                    }
-                    let buttons = document.querySelectorAll("button.save")
-                    for(let i=0;i<buttons.length;i++){
-                        buttons[i].addEventListener("click",(e)=>{
-                         if(!(LOCAL.containLoc(loc[i].place_name))){
-                            
-                            buttons[i].textContent ="saving";
-                            LOCAL.addLoc(loc[i].place_name)
-                            
-                                fetch("/geoweather?lat="+loc[i].geometry.coordinates[1]+"&long="+loc[i].geometry.coordinates[0])
-                                .then((res)=>{
-                                    res.json().then((data)=>{
-                                       console.log(data);
-                                       buttons[i].textContent="saved";buttons[i].style.backgroundColor="green";
-                                       LOCAL.refresh();
-                                    })
-                                    
-                                    })
-                                }  
-                                else{
-                                    LOCAL.addLoc(loc[i].place_name)
-                                }      
-                        })
-                    }
+                let loc = data.features;
+                tabPage.innerHTML="";
+                for(let i=0;i<loc.length;i++){
+                    let div = document.createElement("div");
+                    div.innerHTML = tab(loc[i].place_name,i);
+                    tabPage.appendChild(div);
+                }
+                let buttons = document.querySelectorAll("button.save")
+                for(let i=0;i<buttons.length;i++){
+                    buttons[i].addEventListener("click",(e)=>{
+                     if(!(LOCAL.containLoc(loc[i].place_name))){
+                        buttons[i].textContent ="saving..";
+                        LOCAL.addLoc(loc[i].place_name)
+                            fetch("/geoweather?lat="+loc[i].geometry.coordinates[1]+"&long="+loc[i].geometry.coordinates[0])
+                            .then((res)=>{
+                                res.json().then((data)=>{
+                                   console.log(data);
+                                   buttons[i].textContent="saved";
+                                   buttons[i].style.backgroundColor="green";
+                                   //saver weather data..
+                                   LOCAL.refresh();
+                                   })
+                            })
+                    }else{
+                        LOCAL.addLoc(loc[i].place_name)//handeles case ,that's why calling add loc...
+                    }      
+                    })
+                }
                 })
             })
     }
