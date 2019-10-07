898a3f43c85a03498e0969351686d1bc42432eb8
grvcodes <gurv0002@gmail.com>
2019-10-07 16:07:26 +0530

    fixed initial load error

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -107,7 +107,7 @@ window.addEventListener('load',()=>{
     if(state){
         let date = Date().split(" ")[2];
         if(localStorage.getItem('savedWeatherDate')){
-            if (date = localStorage.getItem('savedWeatherDate')){
+            if(date = localStorage.getItem('savedWeatherDate')){
                 let savedData = localStorage.getItem('savedData').split(" ");
                 console.log(savedData)
                 state.forEach((e,i)=>{
@@ -138,7 +138,7 @@ window.addEventListener('load',()=>{
                     })
                  })
             })
-            localStorage.setItem(saveWeatherDate,Date().split(" ")[2]);
+            localStorage.setItem('saveWeatherDate',Date().split(" ")[2]);
             localStorage.setItem('savedData',saveData.join(' '));
         }
     }else{
